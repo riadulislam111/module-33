4d5d3d8c288391862e4c562461a130cd5957b075
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-20 20:37:24 +0600

    added all file update

--- script/app.js
+++ script/app.js
@@ -14,16 +14,83 @@
 // console.log(newPerson2);
 
 
-const person = {
-    name: "hena",
-    age: 39,
-    friends: ["hero alom", "korim"],
-    status: true,
-}
 
-const newPerson = JSON.stringify(person);
-// console.log(person);
+/////////////////
+
+// const person = {
+//     name: "hena",
+//     age: 39,
+//     friends: ["hero alom", "korim"],
+//     status: true,
+// }
+
+// const newPerson = JSON.stringify(person);
+// console.log(newPerson);
 
 
-const newPerson2 = JSON.parse(newPerson);
-console.log(newPerson2);
+// const newPerson2 = JSON.parse(newPerson);
+// console.log(newPerson2);
+
+
+
+// const person = {
+//     name: "hena",
+//     age: 39,
+//     friends: ["hero alom", "korim"],
+//     status: true,
+// };
+
+// const newPerson = JSON.stringify(person);
+// console.log(newPerson);
+// console.log(typeof newPerson);
+
+
+// const newPerson2 = JSON.parse(newPerson);
+// console.log(newPerson2);
+// console.log(typeof newPerson2);
+
+
+// const person2  = {
+//     name: "arif",
+//     age: 25,
+//     friends: ["john", "steve"],
+//     status: false,
+// };
+
+// const newPerson3 = JSON.stringify(person2);
+// console.log(newPerson3);
+
+// const newPerson4 = JSON.parse(newPerson3);
+// console.log(newPerson4);
+
+
+// const person3 = {
+//     name: "xyz",
+//     age: 34,
+//     friends: ["ali", "hasan"],
+//     status: true,
+// };
+
+// const newPerson5 = JSON.stringify(person3);
+// console.log(newPerson5);
+
+// const newPerson6 = JSON.parse(newPerson5);
+// console.log(newPerson6);
+
+
+// const person4 = {
+//     name: "shimul",
+//     age: 35,
+//     friends: ["ahmed", "kamal"],
+//     status: true,
+// };
+
+
+// const newPerson7 = JSON.stringify(person4);
+// console.log(newPerson7);
+
+// const newPerson8 = JSON.parse(newPerson7);
+// console.log(newPerson8);
+
+
+
