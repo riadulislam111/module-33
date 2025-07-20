103db4188d7a643a33619b9a6762d222359b7b06
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-20 22:34:57 +0600

    added all file update

--- script/fetch.js
+++ script/fetch.js
@@ -15,11 +15,86 @@
 
 
 
+  // // const handlerLoadData = () => {
+
+  // //   fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // //   .then((res) => res.json())
+  // //   .then((data) => console.log(data))
+
+
+  // }
+
+////////////////////
+
+  // fetch('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then(res => res.json())
+  //     .then(data => console.log(data))
+
+  
+
+
+
   const handlerLoadData = () => {
-
     fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-
-
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
   }
+
+
+
+  // fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // .then(res => res.json())
+  // .then(data => console.log("Todo 1:", data))
+
+
+
+  // fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // .then(res => res.json())
+  // .then(data => console.log("Todo 2:", data))
+
+
+
+  // fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // .then(res => res.json())
+  // .then(data => console.log("User 1:", data))
+
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// .then(res => res.json())
+// .then(data => console.log("Post 1:", data));
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// .then(res => res.json())
+// .then(data => console.log("Comment 1:", data))
+
+
+
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Fetches todo data from the given URL and populates the
+ * #todo-data container with the todo details.
+ */
+/*******  512f08db-41ee-41ee-bf7f-cab674a4a216  *******/
+const handlerLoadTodo = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("todo-data");
+   container.innerHTML = `
+   <h3> TODO Details</h3>
+   <p><strong>ID:</strong> ${data.id}</p>
+   <p><strong>Title:</strong> ${data.title}</p>
+   <p><strong>Completed:</strong> ${data.Completed ? "Yes" : "No"}</p>
+   
+   
+   `
+  
+  
+  })
+}
