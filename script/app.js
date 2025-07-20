031108984751d8dec9b52cb3462167e6236d702c
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-20 20:17:30 +0600

    added last all file update

--- script/app.js
+++ script/app.js
@@ -12,3 +12,18 @@
 // const newPerson2 = JSON.parse(newPerson);
 
 // console.log(newPerson2);
+
+
+const person = {
+    name: "hena",
+    age: 39,
+    friends: ["hero alom", "korim"],
+    status: true,
+}
+
+const newPerson = JSON.stringify(person);
+// console.log(person);
+
+
+const newPerson2 = JSON.parse(newPerson);
+console.log(newPerson2);
